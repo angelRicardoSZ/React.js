279e49eac8401243dcee110c3ce9afcdf8890be9
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-19 22:10:13 -0500

    events , states

--- todo-app/src/TodoSearch.js
+++ todo-app/src/TodoSearch.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-    return (
-        <input className="TodoSearch" placeholder="Tarea por hacer"/>
 
-    )
+function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState("")
+
+
+    const onsearchValueChange = (event) => {
+        console.log(event.target.value);
+        setSearchValue(event.target.value)
+    }   
+    return [
+        <input className="TodoSearch"
+         placeholder="Tarea por hacer"
+         value={searchValue}
+         onChange={onsearchValueChange}
+         />,
+         <p>{searchValue}</p>
+    ]
 }
 
 export {TodoSearch};
